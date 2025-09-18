4bcf24c7fe289e1571cc495bf333c4b50fd3dd91
박지민 <pjm5489@naver.com>
2025-09-18 19:46:52 +0900

    refactor: 숫자 키보드로 변경

--- app/src/main/java/woowacourse/payments/ui/cardregister/component/CardNumberInputField.kt
+++ app/src/main/java/woowacourse/payments/ui/cardregister/component/CardNumberInputField.kt
@@ -1,12 +1,14 @@
 package woowacourse.payments.ui.cardregister.component
 
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import woowacourse.payments.R
 import woowacourse.payments.ui.cardregister.CardNumberVisualTransformation
@@ -34,6 +36,7 @@ fun CardNumberInputField(
         },
         visualTransformation = CardNumberVisualTransformation(),
         singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
     )
 }
 
